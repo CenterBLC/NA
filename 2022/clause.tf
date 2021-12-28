7e19434b23248be85f79ea9dfa0234dec98cec6c
Oliver Glanz <o.glanz@gmail.com>
2021-12-28 03:12:53 -0500

    Add files via upload
    Initial release of Nestle Aland's 1904 edition enriched with features developed by the CBLC.

---
+++ 2022/clause.tf
@@ -0,0 +1,14 @@
+@node
+@Author=Nestle & Aland
+@Converter=Oliver Glanz
+@Editors=Ulrik Sandborg Petersen, Eliran Wong, Oliver Glanz, BOL
+@Name=NA1904
+@Note=?
+@Source:=https://github.com/biblicalhumanities/Nestle1904/blob/master/morph/Nestle1904.csv
+@Version=1904
+@description=clause
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2021-12-24T01:54:18Z
+
+138067	1
---
+++ 2022/otype.tf
@@ -0,0 +1,18 @@
+@node
+@Author=Nestle & Aland
+@Converter=Oliver Glanz
+@Editors=Ulrik Sandborg Petersen, Eliran Wong, Oliver Glanz, BOL
+@Name=NA1904
+@Note=?
+@Source:=https://github.com/biblicalhumanities/Nestle1904/blob/master/morph/Nestle1904.csv
+@Version=1904
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2021-12-24T01:54:22Z
+
+1-137779	word
+137780-137806	book
+137807-138066	chapter
+138067	clause
+138068	sentence
+138069-146011	verse
