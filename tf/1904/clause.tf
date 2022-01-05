f934966f271484dfe468e9167004d7a8eb138893
Oliver Glanz <o.glanz@gmail.com>
2022-01-05 00:09:21 -0500

    Add files via upload

---
+++ tf/1904/clause.tf
@@ -0,0 +1,14 @@
+@node
+@Author=Nestle & Aland
+@Converter=Oliver Glanz
+@Editors=Ulrik Sandborg Petersen, Eliran Wong, Oliver Glanz, BOL
+@Name=NA1904
+@Note=?
+@Source:=https://github.com/biblicalhumanities/Nestle1904/blob/master/morph/Nestle1904.csv
+@Version=1904
+@description=clause
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2022-01-05T05:03:44Z
+
+138037	1
