653376a8548ab05be70a653e80a8d319fbe4b908
Oliver Glanz <o.glanz@gmail.com>
2022-11-06 23:42:24 -0500

    Add files via upload

--- tf/1904/freq_lemma.tf
+++ tf/1904/freq_lemma.tf
@@ -7,7 +7,7 @@
 @Source:=https://github.com/biblicalhumanities/Nestle1904/blob/master/morph/Nestle1904.csv
 @Version=1904
 @description=number of occurence of a given lexeme
-@valueType=str
+@valueType=int
 @writtenBy=Text-Fabric
 @dateWritten=2022-11-07T04:31:58Z
 
